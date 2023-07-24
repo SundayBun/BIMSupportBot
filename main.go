08f7790121827732c046cd9f76628b7389c65a06
SundayBun <romashka5205@mail.ru>
2023-07-24 11:14:18 +0300

    update docker-compose.yml

--- main.go
+++ main.go
@@ -65,5 +65,5 @@ func GetConfigPath(configPath string) string {
 	if configPath == "docker" {
 		return "./config/config-docker"
 	}
-	return "./config/config-local"
+	return "./config/config-local.yml"
 }
